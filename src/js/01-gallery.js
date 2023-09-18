f03056610fddd888835237a76d0c970ea3005c24
Piotrporadowski <122733517+Piotrporadowski@users.noreply.github.com>
2023-09-18 19:36:16 +0200

    e

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+'use strict'; 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryElements = galleryItems
+  .map(item => {
+    return `<li class="gallery__item">
+     <a class="gallery__link" href="${item.original}">
+       <img
+        class="gallery__image"
+         src="${item.preview}"
+         data-source="${item.original}"
+         alt="${item.description}"
+       />
+     </a>
+   </li>`;
+  })
+  .join('');
+
+galleryList.innerHTML = galleryElements;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
