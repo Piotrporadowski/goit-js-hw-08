4bcd8a987658af1351f87da21cc433faed0137e0
Piotrporadowski <122733517+Piotrporadowski@users.noreply.github.com>
2023-09-19 17:09:14 +0200

    e

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,20 +7,18 @@ const LOCALSTORAGE_KEY = "feedback-form-state";
 let formState = {
     email: '',
     message: ''
-  };
+};
 
 const checkStorage = localStorage.getItem(LOCALSTORAGE_KEY);
-if(checkStorage){
-  formState = JSON.parse(checkStorage);
-  form.elements.email.value = formState.email;
-  form.elements.message.value = formState.message;
+if (checkStorage) {
+    formState = JSON.parse(checkStorage);
+    form.elements.email.value = formState.email;
+    form.elements.message.value = formState.message;
 }
-
-
 
 const throttledSave = throttle((data) => {
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
-  }, 500);
+}, 500);
 
 function saveData(evt) {
     formState[evt.target.name] = evt.target.value;
@@ -28,25 +26,31 @@ function saveData(evt) {
 }
 form.addEventListener("input", saveData);
 
-
-
-
-
 const sentForm = eventSent => {
     eventSent.preventDefault();
     const {
-      elements: { email, message},
+        elements: { email, message },
     } = eventSent.currentTarget;
+
+    const emailValue = email.value.trim(); // Usuń białe znaki z początku i końca
+    const messageValue = message.value.trim();
+
+    if (!emailValue || !messageValue) {
+        alert("Proszę wypełnić wszystkie pola formularza.");
+        return; // Przerywa funkcję, jeśli pola są puste
+    }
+
     const objectData = {
-        email: email.value,
-        message: message.value,
-      };
-      console.log(objectData);
-      localStorage.removeItem(LOCALSTORAGE_KEY);
-      formState = {
+        email: emailValue,
+        message: messageValue,
+    };
+
+    console.log(objectData);
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    formState = {
         email: '',
         message: ''
-      };
-      form.reset();
+    };
+    form.reset();
 }
 form.addEventListener('submit', sentForm);
